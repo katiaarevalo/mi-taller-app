8bf7585f8d742eebb86926837fa5926113a9d372
Diego Molina <77606137+SirDaXll@users.noreply.github.com>
2024-11-03 18:37:47 -0300

    Cambio: Página principal
    Se cambió la página principal para que sea el login en vez del dashboard, puesto a que cuando iniciabas el proyecto no cargaba ningún dato debido a que faltaba verificar las credenciales de sesión de nuevo.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
-import './App.css';
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./pages/login"; 
 import ProtectedRoutes from "./routes/ProtectedRoutes";
 import RouterApp from "./routes/RouterApp"; 
@@ -8,9 +7,13 @@ import RouterApp from "./routes/RouterApp";
 const App = () => {
   return (
     <Routes>
+      {/* Redirigir la ruta predeterminada ("/") a "/login" */}
+      <Route path="/" element={<Navigate to="/login" />} />
+      
       {/* Ruta pública para Login */}
       <Route path="/login" element={<Login />} />
       
+      {/* Rutas protegidas */}
       <Route
         path="/*"
         element={
