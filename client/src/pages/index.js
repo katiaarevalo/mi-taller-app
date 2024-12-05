a6e090100dcbe55f16431c594a6ebfc05b5cf529
JotaSanchez99 <jordandavid.sanchez@alumnos.ulagos.cl>
2024-12-05 10:58:18 -0300

    algo
    algo xD

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Dashboard } from './Dashboard'; /*Analytics -> RouterApp */
+export { default as Dashboard } from './Dashboard';
 export { default as WorkOrders } from './WorkOrders';
 export { default as Vehicles } from './Vehicles';
 export { default as Clients } from './Clients';
@@ -7,6 +7,7 @@ export { default as Login } from './login';
 export { default as Suppliers } from './Suppliers';
 export { default as WorkOrdersCalendar } from './WorkOrdersCalendar';
 export { default as Cotizaciones } from './Cotizaciones';
+export { default as Inventario } from './InventoryPage.js';
 export { default as AccountPayable} from './AccountPayable';
 export { default as Historial } from './Historial';
 export { default as Debtors } from './Debtors';
